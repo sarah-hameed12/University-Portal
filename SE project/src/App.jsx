cf3aa0efb67903387ab8d1060192b019369bf1d3
mahad bin aamir <26100249@lums.edu.pk>
2025-03-08 06:36:14 +0500

    socities page done

--- SE project/src/App.jsx
+++ SE project/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WelcomePage from "./welcome.jsx";
 import SignIn from "./signin.jsx";
 import Signup from "./signup2.jsx";
+import SocietyScreen from "./societies.jsx";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route index element={<WelcomePage />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup2" element={<Signup />} />
+        <Route path="/society" element={<SocietyScreen />} />
       </Routes>
     </Router>
   );
